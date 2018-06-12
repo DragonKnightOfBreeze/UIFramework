1ed0855e4a2dc5d8a8515ce3d0a100a10759d5c0
微风的龙骑士 风游迩 <2944262241@qq.com>
2018-06-12 21:48:03 +0800

    导入资源，建立目录

---
+++ Assets/Scripts/SUIFW/Config/SysDefine.cs
@@ -0,0 +1,37 @@
+﻿/***
+ * 标题：
+ * 框架核心参数
+ *
+ * 功能：
+ * 1.系统常量
+ * 2.全局性方法
+ * 3.系统枚举类型
+ * 
+ * 思路：
+ *
+ *
+ * 改进：
+ * 
+ *
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//using Kernel;
+//using Global;
+
+namespace SUIFW {
+
+	#region 【系统枚举类型】
+
+
+
+	#endregion	
+
+	public class SysDefine : MonoBehaviour {
+
+	}
+}
